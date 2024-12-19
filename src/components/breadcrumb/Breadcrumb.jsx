d51f5b8272c56ca707712462a23bd91cc1cea2c0
cuplprojects <shivom.chandrakala@gmail.com>
2024-12-19 10:56:25 +0530

    added basic files and folders and header banner image

---
+++ src/components/breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const breadcrumb = () => {
+  return (
+    <div>
+      This is breadcurmb.
+    </div>
+  )
+}
+
+export default breadcrumb
---
+++ src/layouts/GuestLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GuestLayout = () => {
+  return (
+    <div>
+      This is guest layout .
+    </div>
+  )
+}
+
+export default GuestLayout
