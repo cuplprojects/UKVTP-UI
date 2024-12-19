e3ce410a4661321d2ea45a1aee62332d8b3a04fd
cuplprojects <shivom.chandrakala@gmail.com>
2024-12-19 11:35:57 +0530

    setup for protected routes and breadcrumb

--- src/layouts/GuestLayout.jsx
+++ src/layouts/GuestLayout.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import Navbar from '../components/navbar/Navbar'
+import React from 'react';
+import Breadcrumb from '../components/breadcrumb/Breadcrumb';
+
 const GuestLayout = () => {
     return (
         <div>
-            This is guest layout .
+            <header>
+                <h1>Welcome to Our App</h1>
+            </header>
+            
+            <div className="container mt-3">
+                <Breadcrumb />
+                <main className="mt-3">
+                    {/* Add your login form or guest content here */}
+                    <div>
+                        <h2>Login</h2>
+                        {/* Add your login form */}
+                    </div>
+                </main>
+            </div>
+            
+            <footer>
+                <p>Guest Layout Footer</p>
+            </footer>
         </div>
-    )
-}
+    );
+};
 
-export default GuestLayout
+export default GuestLayout;
--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Breadcrumb from '../components/breadcrumb/Breadcrumb';
 
 const UserLayout = () => {
   return (
     <div>
-      This is user layout.
+      <header>
+        {/* Add your header/navbar here */}
+        <h1>User Layout Header</h1>
+      </header>
+      
+      <div className="container mt-3">
+        <Breadcrumb />
+        <main className="mt-3">
+          {/* This is where nested routes will be rendered */}
+          <Outlet />
+        </main>
+      </div>
+      
+      <footer>
+        {/* Add your footer here */}
+        <p>User Layout Footer</p>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default UserLayout
+export default UserLayout;
